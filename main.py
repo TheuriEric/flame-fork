958ee1d04bf11aebeb611a7c06e62d9414ff715d
Eric Theuri <tericmail001@gmail.com>
2025-06-26 23:56:16 +0300

    Added core functional concepts

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # Allows all origins, adjust as needed
-    allow_origins=["https://flameandfork.com", "http://localhost:8000"],
+    allow_origins=["https://flameandfork.com", "http://localhost:8000","https://flameandfork.onrender.com/"],
     allow_credentials=True,
     allow_methods=["POST", "GET"],  # Allows all methods, adjust as needed
     allow_headers=["*"],  # Allows all headers, adjust as needed
@@ -81,3 +81,13 @@ async def root():
 async def chatbot(request: ChatRequest):
     response_text = ai_platform.chat(request.prompt)
     return ChatResponse(response=response_text)
+
+#Import from .env
+#Structuring code in what files? 
+#CORS
+#Error handling
+#Docker containerization
+#Live public deployment
+#Testing with postman
+##Frontend connection
+##Bonus-combining information from multiple files
